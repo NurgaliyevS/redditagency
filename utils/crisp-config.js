5abb832100579d66411c402c2db2600dfd8dcb19
NurgaliyevS <nurgasab@gmail.com>
2025-09-11 16:48:52 +0500

    upd

--- utils/crisp-config.js
+++
@@ -1,8 +0,0 @@
-// utils/crisp-config.js
-const configureCrisp = (websiteId) => {
-    // Initialize Crisp
-    window.$crisp = [];
-    window.CRISP_WEBSITE_ID = websiteId;
-  };
-  
-  export default configureCrisp;
